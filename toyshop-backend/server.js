ed5f879d0ac92aea0e5cf2d3d974b23633456d95
deep5804 <21it077@charusat.edu.in>
2025-02-17 15:30:29 +0530

    adde the Review routes for crud in Review

--- toyshop-backend/server.js
+++ toyshop-backend/server.js
@@ -1,45 +1,3 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
-// const cors = require("cors");
-
-// dotenv.config(); // Load environment variables
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json()); // Middleware to parse JSON data
-
-// // MongoDB Connection
-// mongoose
-//   .connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("MongoDB Atlas Connected ✅"))
-//   .catch((err) => console.error("MongoDB Connection Error:", err));
-
-// app.get("/", (req, res) => {
-//   res.send("Toy Shop API is Running 🚀");
-// });
-
-
-// const userRoutes = require("./routes/userRoutes");
-
-
-// app.use("/api/users", userRoutes);
-
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
-
-// module.exports = app;
-
-
-
-// 
-
-
-
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
@@ -71,6 +29,7 @@ const productRoutes = require("./routes/productRoutes");
 const addressRoutes = require("./routes/addressRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 const cartRoutes = require("./routes/cartRoutes"); 
+const reviewRoutes = require("./routes/reviewRoutes");
 
 
 // Use Routes
@@ -79,6 +38,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/address", addressRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/cart", cartRoutes); 
+app.use("/api/reviews", reviewRoutes);
 
 
 // Set Port
