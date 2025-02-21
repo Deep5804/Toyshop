eb518706ba4694bb3c97ea30ec896bdf0f39c9d9
deep5804 <21it077@charusat.edu.in>
2025-02-21 12:03:32 +0530

    updated many things added total amount

--- toyshop-backend/models/Cart.js
+++ toyshop-backend/models/Cart.js
@@ -1,16 +1,20 @@
+
 // const mongoose = require("mongoose");
 
-// const CartSchema = new mongoose.Schema({
-//   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-//   productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
-//   quantity: { type: Number, required: true, min: 1 },
-// },
-// { collection: "Cart" }  // 👈 Force collection name to be "Cart"
-// )
-// ;
+// const CartSchema = new mongoose.Schema(
+//   {
+//     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+//     items: [
+//       {
+//         productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+//         quantity: { type: Number, required: true, min: 1 },
+//       },
+//     ],
+//   },
+//   { collection: "Cart" } // 👈 Force collection name to be "Cart"
+// );
 
 // module.exports = mongoose.model("Cart", CartSchema);
-
 
 
 const mongoose = require("mongoose");
@@ -22,11 +26,18 @@ const CartSchema = new mongoose.Schema(
       {
         productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
         quantity: { type: Number, required: true, min: 1 },
+        price: { type: Number, required: true, min: 0 }, // Price of the product at the time of adding
       },
     ],
+    totalPrice: { type: Number, required: true, default: 0 }, // Total price of the cart
   },
-  { collection: "Cart" } // 👈 Force collection name to be "Cart"
+  { collection: "Cart" }
 );
 
-module.exports = mongoose.model("Cart", CartSchema);
+// Middleware to calculate total price before saving the cart
+CartSchema.pre("save", function (next) {
+  this.totalPrice = this.items.reduce((total, item) => total + item.quantity * item.price, 0);
+  next();
+});
 
+module.exports = mongoose.model("Cart", CartSchema);
